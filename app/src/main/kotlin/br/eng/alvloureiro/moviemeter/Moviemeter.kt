6adf9a266fe73e93560ef680bdb9d2d82ce38508
Andre Loureiro <loureiro.andrew@gmail.com>
2018-06-10 01:32:59 -0400

    [Application]: Add preferences ref to application class
    Signed-off-by: Andre Loureiro <loureiro.andrew@gmail.com>

--- app/src/main/kotlin/br/eng/alvloureiro/moviemeter/Moviemeter.kt
+++ app/src/main/kotlin/br/eng/alvloureiro/moviemeter/Moviemeter.kt
@@ -1,17 +1,28 @@
 package br.eng.alvloureiro.moviemeter
 
 import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
 import br.eng.alvloureiro.moviemeter.di.components.AppComponent
 import br.eng.alvloureiro.moviemeter.di.components.DaggerAppComponent
 import br.eng.alvloureiro.moviemeter.di.modules.AppModule
 
 
 class Moviemeter: Application() {
+
+    private companion object {
+        const val PREF_FILE_NAME = "eng.br.alvloureiro.moviemeter.preferences"
+    }
+
     val component: AppComponent by lazy {
         DaggerAppComponent
                 .builder()
                 .appModule(AppModule(this))
                 .build()
+    }
+
+    val preferences: SharedPreferences by lazy {
+        baseContext.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE)
     }
 
     override fun onCreate() {
